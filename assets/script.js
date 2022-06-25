cee72493b4ebf508d4186be243186bb888cf5fb0
ZaneMJohnson <zanejohnson@MacBook-Pro-Zane.local>
2022-06-25 13:44:39 -0700

    Challenge Update

--- assets/script.js
+++ assets/script.js
@@ -11,55 +11,59 @@ var uppercaseCharacters = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 ];
 
-var passwordLength = window.prompt("How many characters would you like your passeword to contain?")
+var passwordLength = window.prompt("How many characters would you like your password to contain?")
+
+if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be at least 8 characters");
+}
 
 var isNumberCharacters = window.confirm("Do you want any numbers in your password?")
 var isSpecialCharacters = window.confirm("Do you want any special characters in your password?")
 var isLowercaseCharacters = window.confirm("Do you want any lowercase letters in your password?")
 var isUppercaseCharacters = window.confirm("Do you want any uppercase letters in your password?")
 
-if (passwordLength < 8 && passwordLength > 128) {
-    window.alert("Password must contain at least 8 characters")
-} else if (isNumberCharacters !== true && isSpecialCharacters !== true && isLowerCharacters !== true && !isUpperCharacters !== true)
+if (!isNumberCharacters && !isSpecialCharacters && !isLowercaseCharacters && !isUppercaseCharacters) {
     window.alert("Select at least one character!")
-
-var totalCharacterPool = new Array();
+}
 
 var myCharacters = () => {
     var characterArr = [];
 
-    if (isNumberCharacters === true) {
-        totalCharacterPool.push(...numberCharacters);
+    if (isNumberCharacters) {
+        characterArr.push(...numberCharacters);
     }
-    if (isSpecialCharacters === true) {
-        totalCharacterPool.push(...specialCharacters);
+    if (isSpecialCharacters) {
+        characterArr.push(...specialCharacters);
     }
-    if (isLowercaseCharacters === true) {
-        totalCharacterPool.push(...lowercaseCharacters)
+    if (isLowercaseCharacters) {
+        characterArr.push(...lowercaseCharacters)
     }
-    if (isUppercaseCharacters === true) {
-        totalCharacterPool.push(...uppercaseCharacters)
+    if (isUppercaseCharacters) {
+        characterArr.push(...uppercaseCharacters)
     }
 
     return characterArr
 }
 
-var populateCharacters = len => {
+var generatePassword = len => {
     var myPassword = [];
-    var character = myCharacters();
+    var characters = myCharacters()
 
-    for (let i = 0; i << len; i++) {
-        var randomNumberBetween = Math.floor(Math.random() * myCharacters.length);
+    for (let i = 0; i < len; i++) {
+        var randomNumberBetween = Math.floor(Math.random() * characters.length);
         myPassword.push(characters[randomNumberBetween]);
     }
 
     return myPassword.join("")
 }
 
-var writePassword = () => {
-    var password = document.querySelector("#generate");
-    var passwordText = document.querySelector("#password");
-        
-    passwordText.value = populateCharacters(passwordLength)
+var generateBtn = document.querySelector("#generate");
+  
+function writePassword() {
+   var password = generatePassword(passwordLength);
+   var passwordText = document.querySelector("#password");
+ 
+   passwordText.value = password;
 }
 
+generateBtn.addEventListener("click", writePassword);
